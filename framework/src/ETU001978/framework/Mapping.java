41bc18a0f1776a10158b6762583afd0814fca903
HonorJN <129477307+HonorJN@users.noreply.github.com>
2023-07-07 16:08:00 +0200

    Add files via upload

---
+++ framework/src/ETU001978/framework/Mapping.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ETU001978.framework;
+
+/**
+ *
+ * @author PC
+ */
+public class Mapping {
+    String classname;
+    String methode;
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    public String getMethode() {
+        return methode;
+    }
+
+    public void setMethode(String methode) {
+        this.methode = methode;
+    }
+    
+}
